7fc3f0e93f4443ef225d9f66a08aab4b21431675
Kali Prasad Panda <kaliprasadp7@gmail.com>
2026-08-07 15:23:19 +0530

    footer container created

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,7 +1,13 @@
+import Container from "../ui/Container";
+
 export default function Footer() {
     return (
-        <section>
-            Footer
-        </section>
+        <footer className="bg-gray-100 py-4">
+            <Container>
+                <p className="text-center text-sm text-gray-600">
+                    &copy; {new Date().getFullYear()} Kali Prasad Panda. All rights reserved.
+                </p>
+            </Container>
+        </footer>
     );
 }
